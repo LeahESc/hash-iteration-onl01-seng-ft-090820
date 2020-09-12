7b96974a52fa07d86be783de42de869c4437c3a7
Leah Schlackman <leahemily1@gmail.com>
2020-09-12 19:29:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,8 +12,8 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kid_name, age|
-    if age <=12 
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    if age <= 12 
+      puts "Happy Birthday #{kid_name}! You are now #{age} years old!"
       else
     end
   end
